d20900000c592fdd3b873465e28685a5b8c7c938
Camily-Meireles <54316711+Camily-Meireles@users.noreply.github.com>
2021-09-08 14:40:29 -0300

    my first react mobile app

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Meu primeiro Mobile App RN</Text>
+      <Button title ="Já sei construir mobile app"/>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
